cd3b909425ec31d54871f62140463467ec9caa33
Sid-71 <77094778+Sid-71@users.noreply.github.com>
2023-02-10 01:52:38 +0530

    Time: 50 ms (64.06%), Space: 14.6 MB (76.45%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,47 +8,18 @@
  */
 class Solution {
 public:
-    int len(ListNode*head)
-    {
-        int cnt =1;
-        for(ListNode*curr = head;curr->next!= NULL;curr = curr->next)
+    ListNode *getIntersectionNode(ListNode *ha, ListNode *hb) {
+        ListNode*sa = ha;
+        ListNode*sb = hb;
+        ListNode*ta = ha;
+        ListNode*tb = hb;
+        while(sa != sb)
         {
-            cnt++;
+         if(sa == NULL)sa = tb;
+         else sa = sa->next;
+            if(sb == NULL)sb =ta;
+            else sb = sb->next;
         }
-        return cnt;
-    }
-    ListNode* helper(ListNode*h1,int n1,ListNode*h2, int n2)
-    {
-        int diff = n1-n2;
-        int cnt =0;
-        ListNode*curr = h1;
-        for(; cnt!=diff and curr!=NULL ; curr = curr->next)
-        {
-            cnt++;
-        }
-        ListNode*curr2 = h2;
-        while(curr2!= NULL and curr!= NULL)
-        {
-            if(curr2 == curr)
-            {
-                return curr;
-            }
-            curr = curr->next;
-            curr2=curr2->next;
-        }
-        return curr;
-        
-    }
-    
-    ListNode *getIntersectionNode(ListNode *heada, ListNode *headb) {
-        int l1 = len(heada);
-        int l2 = len(headb);
-        if(l1>l2)
-        {
-            return helper(heada,l1,headb,l2);
-        }
-        else {
-            return helper(headb,l2,heada,l1);
-        }
+        return sa;
     }
 };
