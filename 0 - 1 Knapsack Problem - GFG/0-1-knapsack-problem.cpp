6f0a1402781a294d084256d56b1a755f4fa676a0
Sid-71 <77094778+Sid-71@users.noreply.github.com>
2023-02-14 20:04:26 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+//snippet for shortcuts
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+#define pii pair<int, int>
+#define vpii vector<pii>
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define exit exit(0)
+typedef long long ll;
+#define double long double
+#define debug1(x) cerr << #x << ": " << x << endl
+#define debug2(x, y) cerr << #x << ": " << x << " | " << #y << ": " << y << endl
+#define all(a) a.begin(), a.end()
+
+
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       vvi dp(n + 1 , vi(W+1));
+       for(int i=1; i<= n ; i++)
+       {
+           for(int j=1; j<=W; j++)
+           {
+               // incldue 
+              int inc =0;
+               if(wt[i-1] <= j)
+               {
+                   inc = val[i-1] + dp[i-1][j-wt[i-1]];
+               }
+               int exc = dp[i-1][j];
+               dp[i][j] = max(inc,exc);
+           }
+       }
+       return dp[n][W];
+      
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
