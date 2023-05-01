bb89a65f85814bc937cae619fb192ba9ec4c7c82
Sid-71 <77094778+Sid-71@users.noreply.github.com>
2023-05-01 15:31:40 +0530

    Time: 691 ms (46.33%), Space: 139 MB (83.09%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,62 +1,106 @@
-static const auto __optimize__ = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    return 0;
-}();
-class UnionFind{
-    vector<int> id;
-    int size;
-public:
-    UnionFind(int N ): id(N), size(N) {
-        iota(begin(id), end(id), 0);
-    }
-    int find(int a) {
-        return id[a] == a ? a : (id[a] = find(id[a]));
-    }
-    int connected(int a, int b) {
-        return find(a) == find(b);
-    }
-    void connect(int a, int b) {
-        int p = find(a), q = find(b);
-        if (p == q) return;
-        id[p] = q;
-        --size;
-    }
-    int getSize() { return size; }
+class DSU {
+    private:
+        vector<int> parent;
+        vector<int> rank;
+        int components;
+
+    public : 
+        
+        DSU(int n) {
+            parent.resize(n+1);
+            rank.resize(n+1);
+            components = n;
+            for(int i = 1; i<=n; i++) {
+                parent[i] = i;
+            }
+        }
+    
+        int find (int x) {
+            if (x == parent[x]) 
+                return x;
+
+            return parent[x] = find(parent[x]);
+        }
+
+        void Union (int x, int y) {
+            int x_parent = find(x);
+            int y_parent = find(y);
+
+            if (x_parent == y_parent) 
+                return;
+
+            if(rank[x_parent] > rank[y_parent]) {
+                parent[y_parent] = x_parent;
+            } else if(rank[x_parent] < rank[y_parent]) {
+                parent[x_parent] = y_parent;
+            } else {
+                parent[x_parent] = y_parent;
+                rank[y_parent]++;
+            }
+            components--;
+        }
+    
+        bool isSingle() {
+            return components == 1;
+        }
 };
+
 class Solution {
 public:
-    int maxNumEdgesToRemove(int n, vector<vector<int>>& E) {
-        UnionFind a(n), b(n);
-        int ans = 0;
-        for (auto &e : E) {
-            if (e[0] != 3) continue;
-            int u = e[1] - 1, v = e[2] - 1;
-            if (a.connected(u, v)) {
-                ++ans;
-                continue;
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        
+        auto lambda = [&](vector<int>& v1, vector<int>& v2) {
+            return v1[0] > v2[0];
+        };
+        
+        sort(begin(edges), end(edges), lambda);
+        DSU Alice(n);
+        DSU Bob(n);
+        
+        int addedEdge = 0;
+        
+        for (auto &edge: edges) { // For each edge -
+            int type = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+            
+            if(type == 3) {
+                bool add = false;
+                
+                if(Alice.find(u) != Alice.find(v)) {
+                    Alice.Union(u, v);
+                    add = true;
+                }
+                
+                if(Bob.find(u) != Bob.find(v)) {
+                    Bob.Union(u, v);
+                    add = true;
+                }
+                
+                if(add)
+                    addedEdge++;
+                
+            } else if(type == 2) {
+                if(Bob.find(u) != Bob.find(v)) {
+                    Bob.Union(u, v);
+                    addedEdge++;
+                }
+            } else {
+                if(Alice.find(u) != Alice.find(v)) {
+                    Alice.Union(u, v);
+                    addedEdge++;
+                }
             }
-            a.connect(u, v);
-            b.connect(u, v);
         }
-        for (auto &e : E) {
-            if (e[0] != 1) continue;
-            int u = e[1] - 1, v = e[2] - 1;
-            if (a.connected(u, v)) ++ans;
-            else a.connect(u, v);
+        
+        
+        if(Alice.isSingle() && Bob.isSingle()) {
+            return edges.size() - addedEdge;
         }
-        if (a.getSize() != 1) return -1;
-        for (auto &e : E) {
-            if (e[0] != 2) continue;
-            int u = e[1] - 1, v = e[2] - 1;
-            if (b.connected(u, v)) ++ans;
-            else b.connect(u, v);
-        }
-        return b.getSize() != 1 ? -1 : ans;
+        
+        return -1;
     }
 };
-
-
 // pehle groups ko 3 me banalo 
 // agar mai jinko connect karne jarha hu woh already connected hai toh sahi nhi hai unko connect karna 
 
